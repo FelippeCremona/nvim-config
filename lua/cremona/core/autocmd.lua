153e7c54177aff877a951814f290259c1806e878
Felippe Cremona <felippe.mendes@qintess.com>
2024-10-26 16:15:49 -0300

    impde que telescope abra arquivo em modo insert

---
+++ lua/cremona/core/autocmd.lua
@@ -0,0 +1,8 @@
+-- Impede que o Telescope abra um buffer em modo de inclusao
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
